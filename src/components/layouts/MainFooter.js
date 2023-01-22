80381efad51fd54bc8e1f94f06c6f0b213aaef2a
Karlos Ye <58801593+karlosye@users.noreply.github.com>
2023-01-21 22:42:33 -0500

    Remove social media

--- src/components/layouts/MainFooter.js
+++ src/components/layouts/MainFooter.js
@@ -7,9 +7,7 @@ const MainFooter = () => {
         <div className={classes.address}>
           123 Justin Treaudeu St, Ottawa <br />
           123 456 8888 <br />
-          <a href="mailto:emperorsdish@ni-hao.ca">
-            emperorsdish@ni-hao.ca
-          </a>
+          <a href="mailto:emperorsdish@ni-hao.ca">emperorsdish@ni-hao.ca</a>
           <br />
         </div>
         <div className={classes.hours}>
@@ -18,12 +16,7 @@ const MainFooter = () => {
           WEDNESDAY, THURSDAY, AND SUNDAY: 5:30PM - 10:00PM <br />
           FRIDAY AND SATURDAY: 5:30PM - 11:00PM <br />
         </div>
-        <div className={classes["soc-media"]}>
-          <a href="https://twitter.com/" className="fa fa-facebook"></a>
-          <a href="https://www.facebook.com/" className="fa fa-twitter"></a>
-          <a href="https://www.instagram.com/" className="fa fa-instagram"></a>
-          <a href="https://www.wechat.com/" className="fa fa-weixin"></a>
-        </div>
+        <div className={classes["soc-media"]}></div>
       </footer>
     </>
   );
